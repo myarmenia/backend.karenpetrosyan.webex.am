e55342713a6110184e6275dabbcd10af323ed6ff
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-05-20 13:08:41 +0400

    add project

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\MailToOrganization;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MessageController extends BaseController
+{
+    public function sendMessage(Request $request){
+
+            try {
+                $response= Mail::to('gorckaarm@gmail.com')->send(new MailToOrganization([
+                    "organization_name" =>$request->organization_name,
+                    "number_guest" => $request->number_guest,
+                    "message" => $request->message,
+                    "email" => $request->email
+                ]
+
+                ));
+
+                    return  $this->sendResponse( $response,'success');
+              } catch (\Exception $e) {
+
+                  return $e->getMessage();
+              }
+
+
+
+    }
+}
