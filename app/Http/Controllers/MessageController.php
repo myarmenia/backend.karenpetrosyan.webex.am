a4b3649890c9e352de08e7424eae0baa627715b7
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-05-22 12:12:25 +0400

    add mail

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,13 +13,20 @@ class MessageController extends BaseController
         $comingrezultresponse='';
 
             try {
-                $response= Mail::to('gorckaarm@gmail.com')->send(new MailToOrganization([
+                    $response= Mail::to('zakaryan75@yahoo.com')->send(new MailToOrganization([
+                        "fullname" =>$request->fullname,
+                        "number_guest" => $request->number_guest,
+                        "message" => $request->message,
+                        "coming_rezult" => $request->coming_rezult=="yes" ? "Գալու ենք" : "Չենք գալու",
+                    ],
+                 ));
+                Mail::to('Kevine1electrical@gmail.com')->send(new MailToOrganization([
                     "fullname" =>$request->fullname,
                     "number_guest" => $request->number_guest,
                     "message" => $request->message,
                     "coming_rezult" => $request->coming_rezult=="yes" ? "Գալու ենք" : "Չենք գալու",
-                ]
-// ====
+                ],
+
                 ));
 
                     return  $this->sendResponse( $response,'Ձեր պատասխանը հաջողությամբ ուղարկվել է');
